e60a60e0ff4462cb7f35120a63e3b125b9912087
nakedsneak <g1931152@m.sojo-u.ac.jp>
2022-05-07 22:58:43 +0900

    8-2-3のコミット

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function getByLimit(int $limit_count = 10)
+    {
+        //　updated_atでこう順に並べたのち、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
 }
