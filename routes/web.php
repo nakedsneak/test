6e92431ddc6e3980ae736946f7943b213ee720d5
nakedsneak <g1931152@m.sojo-u.ac.jp>
2022-05-08 20:44:38 +0900

    8-3-2の正常なコミット

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/posts', 'PostController@index');
+Route::get('/', 'PostController@index');
 
+Route::get('/posts/{post}', 'PostController@show') ;
 
-Route::get('/', function () {
-    return view('posts/index');
-});
