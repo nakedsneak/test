620bea9bd04e00f4e3cc9f3e08c2cfa1f1c47581
nakedsneak <g1931152@m.sojo-u.ac.jp>
2022-05-29 17:58:37 +0900

    8-4完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Post;
+
+use App\Http\Requests\PostRequest;
 
 class PostController extends Controller
 /**
@@ -28,5 +28,13 @@ class PostController extends Controller
 	{
 		return view('posts/create');
 	}
+	
+	public function store(PostRequest $request, Post $post)
+	{
+		$input = $request['post'];
+		$post->fill($input)->save();
+		return redirect('/posts/' . $post->id);
+	}
+	
 }
 ?>
